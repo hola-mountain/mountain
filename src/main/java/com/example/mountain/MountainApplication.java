1a133af805404908da9cf33e45ab0985670f5448
kang.minjun <kang.minjun@joins.com>
2022-05-16 17:14:25 +0900

    fix : jenkins test 추가

--- src/main/java/com/example/mountain/MountainApplication.java
+++ src/main/java/com/example/mountain/MountainApplication.java
@@ -1,18 +1,14 @@
 package com.example.mountain;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "MOUNTAIN SWAGGER", version = "1.0", description = "Documentation APIs v1.0"))
 @EnableEurekaClient
 public class MountainApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MountainApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MountainApplication.class, args);
+    }
 
 }
