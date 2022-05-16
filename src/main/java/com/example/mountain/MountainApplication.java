104c6d0af5b0a1e14c19d974939e23f3d855d5f4
kang.minjun <kang.minjun@joins.com>
2022-05-16 17:51:36 +0900

    feat : open api 추가

--- src/main/java/com/example/mountain/MountainApplication.java
+++ src/main/java/com/example/mountain/MountainApplication.java
@@ -1,10 +1,14 @@
 package com.example.mountain;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "MOUNTAIN SWAGGER", version = "1.0", description = "Documentation APIs v1.0"))
 @EnableDiscoveryClient
 public class MountainApplication {
     public static void main(String[] args) {
