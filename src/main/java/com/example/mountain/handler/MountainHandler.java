8d6874ffce5f03b6ba2d9ee4283e4fe4a41cfe13
seonghJoo <etkl8888@naver.com>
2022-05-18 21:25:39 +0900

    comment : 주석추가

--- src/main/java/com/example/mountain/handler/MountainHandler.java
+++ src/main/java/com/example/mountain/handler/MountainHandler.java
@@ -20,7 +20,9 @@ public class MountainHandler {
     // get mountain list By Page
     public Mono<ServerResponse> getMountainList(ServerRequest serverRequest){
 
+        // district : 0 이면 전국 -> 전체 조회
         int district= Integer.parseInt(serverRequest.queryParam("district").get());
+        
         String search = serverRequest.queryParam("search").get();
         int pageNum = Integer.parseInt(serverRequest.queryParam("pageNum").get());
         int pageSize = Integer.parseInt(serverRequest.queryParam("pageSize").get());
@@ -29,21 +31,6 @@ public class MountainHandler {
 
 
         Flux<MountainResp> mountainFlux = mountainService.getMountainListPage(district, pageNum, pageSize, sortBy, isAsc, search);
-
-        return ServerResponse.ok()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(mountainFlux, MountainResp.class)
-                .onErrorResume(error -> ServerResponse.badRequest().build());
-    }
-
-    public Mono<ServerResponse> getMountainAll(ServerRequest serverRequest){
-
-        int pageNum = Integer.parseInt(serverRequest.queryParam("pageNum").get());
-        int pageSize = Integer.parseInt(serverRequest.queryParam("pageSize").get());
-        boolean isAsc = Boolean.parseBoolean(serverRequest.queryParam("isAsc").get());
-        String sortBy = serverRequest.queryParam("sortBy").get();
-
-        Flux<MountainResp> mountainFlux = mountainService.getMountainListPage(pageNum, pageSize, sortBy, isAsc);
 
         return ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_JSON)
